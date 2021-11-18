598bb0962ddc950a09700290b28af058eb0444fb
mironoff2007 <mironoff2007@rambler.ru>
2021-11-18 21:35:09 +0300

    Draw circle

--- app/src/main/java/ru/mironov/radialbarchart/CustomView.kt
+++ app/src/main/java/ru/mironov/radialbarchart/CustomView.kt
@@ -2,6 +2,8 @@ package ru.mironov.radialbarchart
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 
@@ -12,14 +14,25 @@ class CustomView(
     defStyleRes: Int
 ) : View(context, attributesSet, defStyleAttr, defStyleRes) {
 
+    val radius=100
+
     constructor(context: Context, attributesSet: AttributeSet?, defStyleAttr: Int) : this(context, attributesSet, defStyleAttr, 0)
 
     constructor(context: Context, attributesSet: AttributeSet?) : this(context, attributesSet, 0)
 
     constructor(context: Context) : this(context, null)
 
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {}
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
 
-    override fun onDraw(canvas: Canvas) {}
+        setMeasuredDimension(radius*2,radius*2)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        val paint= Paint()
+        paint.setColor(Color.BLUE)
+
+        canvas.drawCircle(radius.toFloat(),radius.toFloat(),radius.toFloat(),paint)
+
+    }
 
 }
