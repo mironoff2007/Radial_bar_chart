582d3d3f7b18e4b07b8c0a530e620823b56c1981
Mironov Ury <mironoff2007@rambler.ru>
2021-11-19 02:02:20 +0300

    Draw Arc, input angle with SeekBar

--- app/src/main/java/ru/mironov/radialbarchart/CustomView.kt
+++ app/src/main/java/ru/mironov/radialbarchart/CustomView.kt
@@ -1,11 +1,18 @@
 package ru.mironov.radialbarchart
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
+import android.graphics.RectF
+
+import android.graphics.CornerPathEffect
+
+
+
 
 class CustomView(
     context: Context,
@@ -14,7 +21,13 @@ class CustomView(
     defStyleRes: Int
 ) : View(context, attributesSet, defStyleAttr, defStyleRes) {
 
-    val radius=100
+    var radius=100
+    val mX=radius*1F
+    val mY=radius*1F
+
+    var sweepAngle=0F
+
+    var strokeWidth=20F
 
     constructor(context: Context, attributesSet: AttributeSet?, defStyleAttr: Int) : this(context, attributesSet, defStyleAttr, 0)
 
@@ -27,11 +40,43 @@ class CustomView(
         setMeasuredDimension(radius*2,radius*2)
     }
 
+
+    fun setAngle(sweepAngle:Float){
+        this.sweepAngle=sweepAngle
+        this.invalidate()
+    }
+
+    @SuppressLint("DrawAllocation")
     override fun onDraw(canvas: Canvas) {
         val paint= Paint()
-        paint.setColor(Color.BLUE)
+        paint.color = Color.BLUE
 
         canvas.drawCircle(radius.toFloat(),radius.toFloat(),radius.toFloat(),paint)
+
+        paint.style = Paint.Style.STROKE
+        paint.strokeWidth = strokeWidth
+
+        paint.color = Color.BLUE
+
+        paint.color = Color.RED
+        paint.isDither = true // set the dither to true
+
+        paint.style = Paint.Style.STROKE // set to STOKE
+
+        paint.strokeJoin = Paint.Join.ROUND // set the join to round you want
+
+        paint.strokeCap = Paint.Cap.ROUND // set the paint cap to round too
+
+        //paint.setPathEffect(CornerPathEffect(50F)) // set the path effect when they join.
+
+        paint.isAntiAlias = true
+
+        val radiusArc=(radius-strokeWidth/2)
+
+        val oval = RectF(mX - radiusArc, mY - radiusArc, mX + radiusArc, mY +radiusArc)
+        canvas.drawArc(oval, -180F, sweepAngle*90/100, false, paint)
+
+        //canvas.drawArc(oval, -90F, 89F, false, paint)
 
     }
 
--- app/src/main/java/ru/mironov/radialbarchart/MainActivity.kt
+++ app/src/main/java/ru/mironov/radialbarchart/MainActivity.kt
@@ -2,11 +2,47 @@ package ru.mironov.radialbarchart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.SeekBar
+import ru.mironov.radialbarchart.databinding.ActivityMainBinding
+import android.widget.Toast
+
+import android.widget.SeekBar.OnSeekBarChangeListener
+
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var customView:CustomView
+
+    private lateinit var bar:SeekBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+        customView=binding.customView
+        bar=binding.seekBar
+
+        bar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+               customView.setAngle(progress*1F)
+            }
+        })
     }
 }
+
